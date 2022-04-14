ed51bd3ab8623cdc17c07215cf920ec0623bbe05
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2022-04-14 14:57:14 +0000

    Update build script

--- build_srw_app.sh
+++ build_srw_app.sh
@@ -1,28 +1,117 @@
 #!/bin/bash
 
 ###########################################################################
+##                                                                       ##
 ## Script to build UFS Short-Range Weather Application (UFS SRW App)     ##
 ##                                                                       ##
-## Usage:                                                                ##
-##  1. Non-coupled (FV3 stand-alone) regional modeling:                  ##
-##             ./build_srw_app.sh                                        ##
-##        or   ./build_srw_app.sh FV3                                    ##
-##        or   ./build_srw_app.sh "FV3"                                  ##
+## Usage (basic examples):                                               ##
+##  1. Non-coupled (ATM stand-alone) regional modeling:                  ##
+##          ./build_srw_app.sh                                           ##
+##       or ./build_srw_app.sh --app=ATM                                 ##
 ##                                                                       ##
-##  2. Coupled regional air quality modeling (RRFS-CMAQ):                ##
-##             ./build_srw_app.sh AQM                                    ##
-##        or   ./build_srw_app.sh "AQM"                                  ##
+##  2. Coupled regional air quality modeling (RRFS-CMAQ=ATM+AQM):        ##
+##          ./build_srw_app.sh --app=AQM                                 ##
 ##                                                                       ##
 ###########################################################################
 
-set -eu
+## Usage setting ==========================================================
+usage () {
+cat << EOF_USAGE
+Usage: $0 [OPTIONS]...
+OPTIONS
+  -h, --help
+      show this help guide
+  --app=APPLICATION
+      SRW App application to build
+      (e.g. ATM for non-coupled AQM stand-alone; AQM for ATM+AQM)
+  --platform=PLATFORM
+      name of machine you are building on
+      (e.g. hera | orion | wcoss_dell_p3)
+  --compiler=COMPILER
+      compiler to use; default depends on platform
+      (e.g. intel | gnu )
+  --ccpp="CCPP_SUITE1,CCPP_SUITE2..."
+      CCCP suites to include in build; delimited with ','
+  --extrn=EXTERNALS
+      Check out external components (YES or NO)
+EOF_USAGE
+}
 
+# Print out usage error and exit
+usage_error () {
+  printf "ERROR: $1\n" >&2
+  usage >&2
+  exit 1
+}
+
+# Print out usage options
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+  usage
+  exit 0
+fi
+
+## Default settings =======================================================
+APPLICATION="ATM"
+PLATFORM="hera"
+COMPILER="intel"
+CCPP_SUITES=""
+EXTERNALS="YES"
+
+## Update of optional arguments ===========================================
+while :; do
+  case $1 in
+    --help|-h) usage; exit 0 ;;
+    --app=?*) APPLICATION=${1#*=} ;;
+    --app|--app=) usage_error "$1 requires argument." ;;
+    --platform=?*) PLATFORM=${1#*=} ;;
+    --platform|--platform=) usage_error "$1 requires argument." ;;
+    --compiler=?*) COMPILER=${1#*=} ;;
+    --compiler|--compiler=) usage_error "$1 requires argument." ;;
+    --ccpp=?*) CCPP_SUITES=${1#*=} ;;
+    --ccpp|--ccpp=) usage_error "$1 requires argument." ;;
+    --extrn=?*) EXTERNALS=${1#*=} ;;
+    --extrn|--extrn=) usage_error "$1 requires argument." ;;
+    -?*|?*) usage_error "Unknown option $1" ;;
+    *) break
+  esac
+  shift
+done
+
+## Ensure uppercase / lowercase ===========================================
+APPLICATION="${APPLICATION^^}"
+PLATFORM="${PLATFORM,,}"
+COMPILER="${COMPILER,,}"
+EXTERNALS="${EXTERNALS^^}"
+
+## Ensure platform name from variance =====================================
+if [[ "${PLATFORM}" == "wcoss_dell" || "${PLATFORM}" == "wcoss1" ||
+      "${PLATFORM}" == "venus" || "${PLATFORM}" = "mars" ||
+      "${PLATFORM}" == "wcoss_dell_p35" ]]; then
+  PLATFORM="wcoss_dell_p3"
+fi
+
+## Print out parameter values =============================================
+echo "Application        :" ${APPLICATION}
+echo "Platform (machine) :" ${PLATFORM}
+echo "Compiler           :" ${COMPILER}
+if [ ! -z "${CCPP_SUITES}" ]; then
+  echo "CCPP_SUITES        :" ${CCPP_SUITES}
+else
+  echo "CCPP_SUITES        : Default in src/CMakeLists.txt"
+fi
+echo "External checkout  :" ${EXTERNALS}
+
+
+###########
+set -eu
+##########
+
+## Directories ============================================================
 if [[ $(uname -s) == Darwin ]]; then
   readonly MYDIR=$(cd "$(dirname "$(greadlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
 else
   readonly MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
 fi
-
 SRW_APP_DIR="${MYDIR}"
 AQM_DIR="${SRW_APP_DIR}/AQM"
 BUILD_DIR="${SRW_APP_DIR}/build"
@@ -30,40 +119,13 @@ BIN_DIR="${SRW_APP_DIR}/bin"
 LIB_DIR="${SRW_APP_DIR}/lib"
 MOD_DIR="${SRW_APP_DIR}/env"
 SRC_DIR="${SRW_APP_DIR}/src"
-## Output file name to check if executables exist in BIN_DIR --------------
+
+## Output file name to check executables in BIN_DIR =======================
 BUILD_OUT_FN="build_exec_pass.out"
 
-###########################################################################
-## User specific parameters                                              ##
-###########################################################################
-## ------------------------------------------------------------------------
-## Forecast model options ("FV3" or "AQM")
-##    FV3  : FV3 stand-alone
-##    AQM  : FV3 + AQM
-## ------------------------------------------------------------------------
-FCST_opt="${1:-FV3}"
-## ------------------------------------------------------------------------
-## CCPP Suites: 
-## For use of the default list (src/CMakeLists.txt): CCPP_SUITES=""
-## ------------------------------------------------------------------------
-if [ ${FCST_opt} = "AQM" ]; then
-  CCPP_SUITES="FV3_GFS_v15p2,FV3_GFS_v16"
-fi
-## Compiler ---------------------------------------------------------------
-COMPILER="intel"
-## ------------------------------------------------------------------------
-## Clone components ("YES" or else)
-##    YES : check out external components from GitHub
-## ------------------------------------------------------------------------
-clone_externals="YES"
-##
-###########################################################################
-##
-###########################################################################
-## List of all executables                                               ##
-###########################################################################
-##
-## Basic executables of the UFS SRW App -----------------------------------
+
+### List of Excutables ### ================================================
+## Basic executables of the UFS SRW App ===================================
 declare -a exec_srw=( chgres_cube \
                       emcsfc_ice_blend \
                       emcsfc_snow2mdl \
@@ -84,24 +146,12 @@ declare -a exec_srw=( chgres_cube \
                       ufs_model \
                       upp.x \
                       vcoord_gen )
-
-## Additional executables for AQM -----------------------------------------
+## Additional executables for AQM =========================================
 declare -a exec_aqm=( nexus \
                       gefs2lbc_para )
-##
-###########################################################################
 
-echo "Forecast model option     :" ${FCST_opt}
-if [ ! -z "${CCPP_SUITES}" ]; then
-  echo "CCPP_SUITES               :" ${CCPP_SUITES}
-else
-  echo "CCPP_SUITES               : Default"
-fi
-
-echo "Compiler                  :" ${COMPILER}
-echo "Clone external components :" ${clone_externals}
-
-if [ "${clone_externals}" = "YES" ]; then
+## Remove build-related directories when EXTERNALS=YES ====================
+if [ "${EXTERNALS}" = "YES" ]; then
   rm -rf ${BIN_DIR}
   rm -rf ${BUILD_DIR}
   rm -rf ${SRW_APP_DIR}/include
@@ -110,36 +160,33 @@ if [ "${clone_externals}" = "YES" ]; then
   rm -f ${BUILD_OUT_FN}
 fi
 
-## detect PLATFORM (MACHINE) ----------------------------------------------
-source ${MOD_DIR}/detect_machine.sh
-
-## Check out the external components --------------------------------------
-if [ "${clone_externals}" = "YES" ]; then
+## Check out external components ==========================================
+if [ "${EXTERNALS}" = "YES" ]; then
   echo "... Checking out the external components ..."
-  if [ "${FCST_opt}" = "FV3" ]; then
+  if [ "${APPLICATION}" = "ATM" ]; then
     ./manage_externals/checkout_externals
-  elif [ "${FCST_opt}" = "AQM" ]; then
+  elif [ "${APPLICATION}" = "AQM" ]; then
     ./manage_externals/checkout_externals -e ${AQM_DIR}/Externals.cfg
   else
-    echo "Fatal Error: forecast model is not on the list."
+    echo "Fatal Error: application is not on the list."
     exit 1
   fi
 fi
 
-## CMAKE settings ---------------------------------------------------------
+## CMAKE settings =========================================================
 CMAKE_SETTINGS="-DCMAKE_INSTALL_PREFIX=${SRW_APP_DIR}"
-if [ "${FCST_opt}" = "AQM" ]; then
+if [ "${APPLICATION}" = "AQM" ]; then
   CMAKE_SETTINGS="${CMAKE_SETTINGS} -DAPP=ATMAQ -DCPL_AQM=ON"
 fi
 if [ ! -z "${CCPP_SUITES}" ]; then
   CMAKE_SETTINGS="${CMAKE_SETTINGS} -DCCPP_SUITES=${CCPP_SUITES}"
 fi
 
-## Make build directory ---------------------------------------------------
+## Make build directory ===================================================
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
-## Build UFS SRW App ------------------------------------------------------
+## Build UFS SRW App ======================================================
 echo "... Load environment file ..."
 MOD_FILE="${MOD_DIR}/build_${PLATFORM}_${COMPILER}.env"
 module use ${MOD_DIR}
@@ -152,20 +199,20 @@ echo "... Compile executables ..."
 make -j8 2>&1 | tee log.make.app
 echo "... App build completed ..."
 
-## Replace UPP control file for AQM -----------------------------------------
-if [ "${FCST_opt}" = "AQM" ]; then
+## Replace UPP control file for AQM =======================================
+if [ "${APPLICATION}" = "AQM" ]; then
   echo "... Replace UPP control file ..."
   cp "${SRC_DIR}/AQM-utils/parm/postxconfig-NT-fv3lam_cmaq.txt" "${SRC_DIR}/UPP/parm/" || exit 1
 fi
 
 cd ${SRW_APP_DIR}
 
-## Create output file to check if executables exist in BIN_DIR ------------
+## Create output file to check if executables exist in BIN_DIR ============
 if [ ! -f "${BUILD_OUT_FN}" ]; then
    touch ${BUILD_OUT_FN}
 fi
 
-## Check if all executables exist in BIN_DIR ------------------------------
+## Check if all executables exist in BIN_DIR ==============================
 n_fail=0
 echo $( date --utc ) >> ${BUILD_OUT_FN}
 for file in "${exec_srw[@]}" ; do
@@ -177,7 +224,7 @@ for file in "${exec_srw[@]}" ; do
     (( n_fail=n_fail+1 ))
   fi
 done
-if [ "${FCST_opt}" = "AQM" ]; then
+if [ "${APPLICATION}" = "AQM" ]; then
   for file in "${exec_aqm[@]}" ; do
     exec_file="${BIN_DIR}/${file}"
     if [ -f ${exec_file} ]; then
